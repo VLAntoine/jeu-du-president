e000a0f16818e3c9de5a0a87d88761ac5fd2b2ce
yanis <yanis.legrand.1@gmail.com>
2022-10-24 16:52:13 +0200

    compare - Card

--- model.py
+++ model.py
@@ -1,11 +1,45 @@
-from multiprocessing.sharedctypes import Value
+from json import detect_encoding
+import random
+from select import select
 import string
-import symbol
+
+suits = ['♡', '♤', '♢', '♧']
+values = ['3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '2']
 
 
-class Card :
-    symbol = string
-    suits = string    
-    value = int 
+class Card:
+    def __init__(self, suit, value):
+        self.__suit: string = suit
+        self.__value = value
 
-    
+    # compare values
+    def __eq__(self, other) -> bool:
+        return self.__value == other.value
+    def __lt__(self, other) -> bool:
+        return self.__value < other.value
+    def __ne__(self, other) -> bool:
+        return self.__value != other.value
+    def __gt__(self, other) -> bool:
+        return self.__value > other.value 
+
+    # Compare suits
+    def __eq__(self, other) -> bool:
+        return self.__value == other.value
+    def __ne__(self, other) -> bool:
+        return self.__value != other.value
+
+class Deck:
+    def __init__(self):
+        self.__cards: list[Card] = []
+        for suit in suits:
+            for value in values:
+                self.__cards.append(Card(suit, value))
+
+        random.shuffle(self.__cards)
+
+class PresidentGame(Player) :
+    def __init__(self) -> None:
+        return 0
+
+    def distribute_cards():
+        
--- test1.py
+++ test1.py
@@ -3,18 +3,18 @@ import model
 
 class TestCardsExercice1(unittest.TestCase):
     def test_card_constructor(self):
-        self.assertTrue(isinstance(models.Card('A', '♡'), models.Card))
+        self.assertTrue(isinstance(model.Card('A', '♡'), model.Card))
 
     def test_cards_equal_value(self):
-        ace_of_hearts = models.Card('A', '♡')
-        ace_of_spades = models.Card('A', '♤')
+        ace_of_hearts = model.Card('A', '♡')
+        ace_of_spades = model.Card('A', '♤')
         self.assertEqual(ace_of_hearts, ace_of_spades, 'Two cards having '
                                                        'same value should be considered equal')
 
     def test_cards_comparison(self):
-        ace_of_hearts = models.Card('A', '♡')
-        two_of_hearts = models.Card('2', '♡')
-        five_of_hearts = models.Card('5', '♡')
+        ace_of_hearts = model.Card('A', '♡')
+        two_of_hearts = model.Card('2', '♡')
+        five_of_hearts = model.Card('5', '♡')
 
         self.assertTrue(ace_of_hearts > five_of_hearts)
         self.assertTrue(two_of_hearts > ace_of_hearts > five_of_hearts,
@@ -24,12 +24,12 @@ class TestCardsExercice1(unittest.TestCase):
 
 class TestDeckExercice1(unittest.TestCase):
      def test_deck_has_52_cards(self):
-         deck = models.Deck()
+         deck = model.Deck()
          self.assertEqual(len(deck.cards), 52, 'The president is a card game '
                                                'requiring 52 cards')
      def test_deck_shuffling(self):
-         deck_1 = models.Deck()
-         deck_2 = models.Deck()
+         deck_1 = model.Deck()
+         deck_2 = model.Deck()
          self.assertEqual(deck_1.cards, deck_2.cards, 'A new deck should not be automatically shuffled')
          deck_2.shuffle()
          self.assertNotEqual(deck_1.cards, deck_2.cards,'Shuffling a deck '
